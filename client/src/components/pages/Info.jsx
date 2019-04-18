264cce40f80b7e26ae177c543b529d663bf6e544
Riley Brown <brown.riley.c@gmail.com>
2019-04-18 12:14:04 -0400

    hey yay

--- client/src/components/pages/Info.jsx
+++ client/src/components/pages/Info.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-// import Nav from "./components/Nav/Nav";
 import Head from "../Head/Head";
 import Para from "../Para/Para";
-import Nav from '../Nav/Nav';
 import TabNav from "../TabNav/TabNav"
 
 function Info(props) {
