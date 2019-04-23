c51dcaada91fc3398807f864da54ff2ee9436612
jonDatz <notjondatz@gmail.com>
2019-04-22 20:04:04 -0400

    Building out and styling a bit. There are placeholder card components I'll be continuing to work on and test out. Seperating concerns. (#13)

--- client/src/components/SNav/SNav.jsx
+++ client/src/components/SNav/SNav.jsx
@@ -1,47 +1,83 @@
-import React, {Component} from "react";
-import { SideNav, SideNavItem } from 'react-materialize';
-import "./style.css"
+import React, { Component } from "react";
+import { SideNav, SideNavItem, Button } from "react-materialize";
+import "./style.css";
 
 class SNav extends Component {
-    goTo(route) {
-        this.props.history.replace(`/${route}`)
-      };
+  goTo(route) {
+    this.props.history.replace(`/${route}`);
+  }
 
-      login() {
-        this.props.auth.login();
-      };
-    
-      logout() {
-        this.props.auth.logout();
-      };
+  login() {
+    this.props.auth.login();
+  }
 
-    render () {
-        const { isAuthenticated } = this.props.auth;
-        return (
-            <div id="snav">
-            <SideNav  options={{closeOnClick: true, draggable: true}} className="sidenav sidenav-fixed grey lighten-2">
+  logout() {
+    this.props.auth.logout();
+  }
 
-            {!isAuthenticated() && (
-                <SideNavItem waves onClick={this.login.bind(this)}><span  className="blue-grey-text text-lighten-5">Login</span></SideNavItem>
-            )}
+  render() {
+    const { isAuthenticated } = this.props.auth;
+    return (
+      <div id="snav">
+        <SideNav
+          options={{ closeOnClick: true, draggable: true }}
+          className="sidenav sidenav-fixed"
+        >
+          <div className="title">
+            <h2 className="red-text text-accent-4 title-1">The Rules...</h2>
+            <h4 className="title-2">
+              <strong>& Everything Else</strong>
+            </h4>
+          </div>
+          <hr className="divider" />
+          <SideNavItem waves onClick={this.goTo.bind(this, "")}>
+            <span className="red-text text-accent-4 sLinks">Home</span>{" "}
+          </SideNavItem>
+          <hr className="divider" />
+          <SideNavItem waves onClick={this.goTo.bind(this, "compendium")}>
+            <span className="red-text text-accent-4 sLinks">
+              Beginner's Compendium
+            </span>
+          </SideNavItem>
+          <hr className="divider" />
 
-            {isAuthenticated() && (
-                <SideNavItem waves onClick={this.logout.bind(this)}><span  className="blue-grey-text text-lighten-5">Logout</span></SideNavItem>
-            )}
+          {isAuthenticated() && (
+            <React.Fragment>
 
-                <SideNavItem onClick={this.goTo.bind(this, '')}><span  className="blue-grey-text text-lighten-5">Home</span> </SideNavItem>
+                <SideNavItem waves onClick={this.goTo.bind(this, "charactersheet")}>
+                  <span className="red-text text-accent-4 sLinks">
+                    Custom Character Sheet
+                  </span>
+                </SideNavItem>
+                <hr className="divider" />
 
-                <SideNavItem onClick={this.goTo.bind(this, 'compendium')}><span  className="blue-grey-text text-lighten-5">Beginner's Compendium</span></SideNavItem>
+            </React.Fragment>
+          )}
 
-            {isAuthenticated() && (
-                <SideNavItem waves onClick={this.goTo.bind(this, 'charactersheet')}><span  className="blue-grey-text text-lighten-5">Custom Character Sheet</span></SideNavItem>
-            )}
-                
+          
+          {!isAuthenticated() && (
+            <SideNavItem waves onClick={this.login.bind(this)}>
+                <Button flat waves="light" className="login">
+                Login
+                </Button>
+            </SideNavItem>
+          )}
 
-            </SideNav>
-            </div>
-        )
-    }
+          {isAuthenticated() && (
+
+              <Button onClick={this.logout.bind(this)}  flat waves="light" className="login">
+                Logout
+              </Button>
+
+          )}
+
+
+
+
+        </SideNav>
+      </div>
+    );
+  }
 }
 
 export default SNav;
